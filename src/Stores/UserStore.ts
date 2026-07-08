d2739497acdff62231611679d6e14a5b891de4bd
Stephen <60153794+stephenmhudak@users.noreply.github.com>
2026-07-08 00:48:54 -0500

    Add user info form

--- src/Stores/UserStore.ts
+++ src/Stores/UserStore.ts
@@ -5,16 +5,10 @@ export const useUserStore = defineStore('User', () => {
     const firstName = ref<string>('');
     const lastName = ref<string>('');
     const age = ref<number>(0);
-    const gender = ref<"" | "Male" | "Female">('');
-    const highestLevelOfEducation = ref<"" | "Some High School" | "High School Diploma" | "Some College" | "Associate's Degree" | "Bachelor's Degree or Higher">('');
-    const race = ref<"" | "American Indian or Alaskan Native" | "Asian" | "Black or African American" | "Hispanic or Latino" | "Middle Eastern of North African" | "Native Hawaiian or Pacific Islander" | "White">(''); //https://www.census.gov/about/our-research/race-ethnicity/standards-updates.html
 
     return { 
         firstName,
         lastName,
-        age,
-        gender,
-        highestLevelOfEducation,
-        race
+        age
     }
 })
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import Home from './Pages/Home.vue'
+import New from './Pages/New/index.vue'
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/new', component: New },
 ]
 
 export const router = createRouter({
